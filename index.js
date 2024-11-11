d6187eb3d26bedc669041fd8a0a8a1e03422d908
AdityaGaur7 <adgaur027@gmail.com>
2024-11-11 13:49:25 +0530

    jao bhai

--- index.js
+++ index.js
@@ -29,6 +29,9 @@ mongoose.connect(process.env.MONGO_URI)
 // Middleware
 app.use(bodyParser.json());
 app.use('/api/users', userRoutes);
+app.use('/', (req, res) => {
+  res.send('API is running...');
+});
 app.use('/api/products', productRoutes);
 app.use(errorMiddleware); // Global error handler
 
